7d774bb103cb41dc6ec9a403e2edceeab5718742
= <=>
2021-08-04 08:05:16 +0700

    :truck: Add Source PPDB

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/orders/ppdb/';
+$config['base_url'] = 'http://localhost/orders/github/ppdb/';
 
 /*
 |--------------------------------------------------------------------------
